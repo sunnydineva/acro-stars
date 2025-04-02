8f589785c02fa30cc782a3df97c4e7ea03da6bd9
Sunny Dineva <sunny.dineva@uptempo.io>
2025-04-03 02:39:13 +0300

    first commit

---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+  //     document.getElementById("header").innerHTML = "fjfhgkjsdhjg";
+    });
+});
