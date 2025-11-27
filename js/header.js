1d7c56eba67dbb8808240b6fc1a2d853b120f539
Sunny Dineva <sunny.dineva@uptempo.io>
2025-11-27 23:28:55 +0200

    Next.js - init commit

--- js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("header").innerHTML = data;
-  //     document.getElementById("header").innerHTML = "fjfhgkjsdhjg";
-    });
-});
---
+++ nextjs-app/components/FacebookFeed.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Script from "next/script";
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function FacebookFeed() {
+  return (
+    <>
+      <section className={styles.mainFbFeed}>
+        <div className={`montserrat-ExtraBold-title ${styles.acroTrails}`}>
+          По следите <br/>
+          на отбора
+        </div>
+
+        <div className={styles.fbBox}>
+          <div id="fb-root"></div>
+          <div
+            className="fb-page"
+            data-href="https://www.facebook.com/profile.php?id=100063250867590"
+            data-tabs="timeline"
+            data-width="700"
+            data-height="480"
+            data-small-header="true"
+            data-adapt-container-width="true"
+            data-hide-cover="false"
+            data-show-facepile="false"
+          >
+            <blockquote
+              cite="https://www.facebook.com/profile.php?id=100063250867590"
+              className="fb-xfbml-parse-ignore"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063250867590">
+                СКА &quot;Акро Старс&quot;
+              </a>
+            </blockquote>
+          </div>
+        </div>
+      </section>
+
+      {/* Facebook SDK Script – с next/script */}
+      <Script
+        id="fb-sdk"
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v19.0"
+      />
+    </>
+  );
+}
