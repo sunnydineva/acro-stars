fb7993ced2e5a36bc5b16ede26cfd31f05bbe6df
Sunny Dineva <sunny.dineva@uptempo.io>
2025-11-30 22:07:00 +0200

    Deploy out fix

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",          // казваме на Next да прави статичен export в ./out
@@ -5,9 +7,9 @@ const nextConfig = {
     unoptimized: true,       // нужно за GitHub Pages (без image optimization)
   },
   // repo-то ти в GitHub  URL ще е https://username.github.io/Acro-Stars/
-  // тогава basePath и assetPrefix трябва да са "/Acro-Stars"
-  basePath: "/Acro-Stars",
-  assetPrefix: "/Acro-Stars/",
+  // тогава basePath и assetPrefix трябва да са "/acro-stars"
+  basePath: isProd ? "/acro-stars" : "",
+  assetPrefix: isProd ? "/acro-stars" : "",
 };
 
 module.exports = nextConfig;
